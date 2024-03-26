d8e1d4a3819fdbaff73c08c3dba0dbe368b65922
machine-afk123 <serraojoshua@gmail.com>
2024-03-26 11:38:26 +0800

    prelim code for indexer.py

---
+++ backend_code/indexer.py
@@ -0,0 +1,17 @@
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+# PROCESS:
+# 1. Remove stopwords
+# 2. Transform words into stems
+# 3. Insert stems into two inverted files.
+
+def stopword_remover(keywords):
+    # TODO: Remove stopwords
+
+def stemmer(words):
+    # TODO: Stem the words
+
+def index(results):
+    # TODO: Apply stopword_remover, stemmer
+    # Then insert into two inverted files.
