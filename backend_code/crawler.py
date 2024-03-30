e6e52f0757e90a1597af1241af7f0c009f9f1a81
godly-recluse <63873623+godly-recluse@users.noreply.github.com>
2024-03-30 12:20:01 +0800

    Some progress on Indexer

--- backend_code/crawler.py
+++ backend_code/crawler.py
@@ -12,11 +12,7 @@ class Crawler:
         self.num_pages = num_pages
         self.db_conn = sqlite3.connect('web_crawler.db')
         self.c = self.db_conn.cursor()
-        # self.title
-        # self.body_content
-        # self.last_modified_date
-        # self.page_size
-        # self.child_links
+
     def page_id_exists(self, url):
         self.c.execute("SELECT page_id FROM page_mapping WHERE url = ?", (url,))
         page_id = self.c.fetchone()
@@ -90,7 +86,7 @@ class Crawler:
         
         return visited_urls
     
-    def crawl(self, url, crawl_complete):
+    def crawl(self, url):
         try:
             # TODO: DEFINE RULES FOR FETCHING
             page_id = self.page_id_exists(url)
@@ -98,7 +94,6 @@ class Crawler:
                 return None
             response = requests.get(url) # make get request to link to get the data.
             response.raise_for_status()
-            page_size = len(response.content)
             
             beautiful_soup = BeautifulSoup(response.content, "html.parser")
             page_counter = 0
@@ -106,8 +101,6 @@ class Crawler:
             date_result = self.last_modified_date_checker(url, last_modified_date)
             if date_result == False:
                 return None
-                
-            title, body_text = self.get_content(beautiful_soup)
 
             links = []
             for link in beautiful_soup.find_all("a", href=True):
@@ -115,35 +108,7 @@ class Crawler:
                 links.append(child_link)
                 page_counter += 1
 
-            if crawl_complete:
-                print(f"URL: {url}")
-                print(f"Page Size: {page_size}")
-                print(f"Title: {title}")
-                print(f"Last Modified: {last_modified_date}")
-                print(f"Body: {body_text}")
-                print(f"Child links: {links} \n")
-
             return links
         
         except requests.exceptions.RequestException:
             return None
-
-if __name__ == "__main__":
-    extracted_links = []
-    base_url = "https://www.cse.ust.hk/~kwtleung/COMP4321/testpage.htm"
-    num_pages = 30
-    crawler = Crawler(base_url, num_pages)
-    bfs_extract = crawler.bfs_extract()
-
-    # print(f"Extracted links: ")
-    # i = 0
-    # # bfs_extract = sorted(bfs_extract)
-    # for x in bfs_extract:
-    #     i += 1
-    #     print(f"{i}. {x}")
-
-    page_no = 1
-    for link in bfs_extract:
-        print(f"Page {page_no}")
-        page_no += 1
-        crawl = crawler.crawl(link, True)
--- backend_code/indexer.py
+++ backend_code/indexer.py
@@ -1,7 +1,11 @@
 # from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from nltk.tokenize import word_tokenize
+import requests
+from bs4 import BeautifulSoup
+import json
 import crawler
+import db
 
 # PROCESS:
 # 1. Remove stopwords
@@ -30,7 +34,7 @@ def stopword_remover(text):
     return filtered_words
 
 def stemmer(filtered_words):
-    #
+    # TODO: Stem the filtered words
     porter_stemmer = PorterStemmer()
     stemmed_words = []
     for word in filtered_words:
@@ -43,16 +47,86 @@ def stemmer(filtered_words):
     return stemmed_words
 
 def index(crawler_text):
-    #
-    # apply stopword removal
+    # apply stopword removal and stemming
     filtered_words = stopword_remover(crawler_text)
     preprocessed_words = stemmer(filtered_words)
 
     return preprocessed_words
 
 def create_index():
-    #
-    web_crawler = crawler.Crawler()
-    crawled_result = web_crawler.get_content()
+    # page_mapping - populate_mapping(attribute, "page_mapping") - one by one
+    # stemmed_mapping - populate_mapping(attribute, "stemmed_mapping") - one by one
+    # non_stemmed_mapping - populate_mapping(attribute, "non_stemmed_mapping") - one by one
+
+    # page_info - populate_pageinfo(pages) - dictionary of dictionaries
+    # pages = {
+    #     'page1': {
+    #         'page_size': 200,
+    #         'last_modified': '2022-01-01',
+    #         'title': 'Page 1',
+    #         'body': 'This is page 1',
+    #         'child_links': ['url2', 'url3']
+    #     },
+    #     'page2': {
+    #         'page_size': 100,
+    #         'last_modified': '2022-01-02',
+    #         'title': 'Page 2',
+    #         'body': 'This is page 2',
+    #         'child_links': ['url1']
+    #     }
+    # }
+    
+    # forwardIndex_body - populate_forward_index(page_id, word_id, frequency, positions, "forwardIndex_body") - one by one
+    # forwardIndex_title - populate_forward_index(page_id, word_id, frequency, positions, "forwardIndex_title") - one by one
+
+    # invertedIndex_body - populate_inverted_index(word_id, page_body_freq, "invertedIndex_body") - one by one
+    # invertedIndex_title - populate_inverted_index(word_id, page_title_freq, "invertedIndex_title") - one by one
+    # pages = {
+    #     'page1': [3, [1, 4, 15]],
+    #     'page2': [2, [21, 37]],
+    # }
+    # page_body_freq = json.dumps(pages)
+
+    base_url = "https://www.cse.ust.hk/~kwtleung/COMP4321/testpage.htm"
+    num_pages = 30
+    web_crawler = crawler.Crawler(base_url, num_pages)
+    
+    extracted_links = web_crawler.bfs_extract()
+    extracted_links = sorted(extracted_links)
+    text = ""
+    pages = {}
+    for page_url in extracted_links:
+        response = requests.get(page_url) # make get request to link to get the data.
+        response.raise_for_status()
+        page_size = len(response.content)
+        beautiful_soup = BeautifulSoup(response.content, "html.parser")
+        title, body = web_crawler.get_content(beautiful_soup)
+        pages[page_url] = {
+            'page_size': page_size,
+            'last_modified': web_crawler.get_last_modified_date(page_url),
+            'title': title,
+            'body': body,
+            'child_links': web_crawler.crawl(page_url)
+        }
+        text += title + " " + body + " "
+    
+    non_stemmed_words = stopword_remover(text)
+    for word in non_stemmed_words:
+        db.populate_mapping(word, "non_stemmed_mapping")
+    
+    stemmed_words = stemmer(non_stemmed_words)
+    for word in stemmed_words:
+        db.populate_mapping(word, "stemmed_mapping")
+
+    for url in pages:
+        db.populate_mapping(url, "page_mapping")
+
+    db.populate_pageinfo(pages)
+
+    
+         
+
+    
+
 
 
